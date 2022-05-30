8fe0838bb2b1ac7a59ec630dd97635c6469e7095
ashit452 <ajpatel2468@gmail.com>
2022-05-30 17:58:44 +0530

    working on attribute

--- Project/Attribute/admin.py
+++ Project/Attribute/admin.py
@@ -3,8 +3,26 @@ from Attribute.models import attribute
 from Option.models import option
 from AttributeTranslation.models import attributeTranslation
 from OptionTranslation.models import optionTranslation
+from django.http import HttpResponseRedirect
+from Language.models import language
+
+from django.contrib import messages
+from django.template.defaulttags import register
+...
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
 # Register your models here.
-admin.site.register(attribute)
-admin.site.register(option)
-admin.site.register(attributeTranslation)
-admin.site.register(optionTranslation)
+class AttributeAdmin(admin.ModelAdmin):
+    def changeform_view(self, request,obj ,form_url,context=None):
+        context = context or {}
+        languageData = language.objects.filter(status = "enabled")
+        context["language"]=languageData
+        print(context["language"])
+
+        
+        return super(AttributeAdmin,self).changeform_view(request, obj ,form_url,context)
+
+admin.site.register(attribute,AttributeAdmin)
+
