200c0e9c7b6d0c1881eea177f6ba86ee9ecbd15a
ashit452 <ajpatel2468@gmail.com>
2022-07-05 18:10:44 +0530

    Customer and Address modules are done

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -177,8 +177,9 @@ TINYMCE_DEFAULT_CONFIG = {
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = os.environ.get('EMAIL_ID')
-EMAIL_HOST_PASSWORD = os.environ.get('PASS_EMAIL')
+EMAIL_HOST_USER = os.getenv('EMAIL_ID')
+EMAIL_HOST_PASSWORD = os.getenv('PASS_EMAIL')
+
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
